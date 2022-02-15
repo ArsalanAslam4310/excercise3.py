336c92449b833a396f4ec62548db0757e2faf54f
ArsalanAslam4310 <97877778+ArsalanAslam4310@users.noreply.github.com>
2022-02-16 00:01:49 +0500

    Add files via upload

--- loop_11.py
+++ loop_11.py
@@ -1,11 +1,12 @@
 prev_number=0
 current_number=0
 next_number=prev_number+current_number
-
-for i in range(10):
+sum = 0
+while next_number <= 10:
     prev_number=current_number
     current_number=next_number
     next_number= prev_number+current_number
-    print(next_number)
 
-    
+    if next_number%2==0:
+        sum = sum +next_number
+print(sum)
