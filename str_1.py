0a8feddad6d74da471dd91fae0317559f6e379a7
ArsalanAslam4310 <97877778+ArsalanAslam4310@users.noreply.github.com>
2022-02-17 10:31:48 +0500

    Update str_1.py

--- str_1.py
+++ str_1.py
@@ -1,6 +1,6 @@
-def argument(name,age):
+def print_bio(name,age):
     print(name,age)
 
-name = input("Enter a name:")
-age=int(input("Enter a age"))
-argument(name,age)
+name = input("Enter your name:")
+age=int(input("Enter your age"))
+print_bio(name,age)
