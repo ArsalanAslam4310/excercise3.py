b428ec5e58d1064784481d883cac2040228b349e
ArsalanAslam4310 <97877778+ArsalanAslam4310@users.noreply.github.com>
2022-02-18 11:44:14 +0500

    Update sort.py

--- sort.py
+++ sort.py
@@ -1,7 +1,20 @@
-list_of_number=[56,6562,5455,555,2202,52]
+def find_index(list_of_number,start,value):
+    for i in range(start,len(list_of_number)):
+        if list_of_number[i] == value:
+            return i
+
+def bubble_sort(list_of_numbers,order=True):
+
+    for i in range(len(list_of_numbers)):
+        min_index=find_index(list_of_numbers,i,min(list_of_numbers[i:])) #index of the minimum number in the list
+        
+        list_of_numbers[i],list_of_numbers[min_index]=list_of_numbers[min_index],list_of_numbers[i]
+
+    if order==False:
+        list_of_numbers.reverse()
+
+    return list_of_numbers   
 
 
-for index in range(len(list_of_number)):
-    min_index=list_of_number.index(min)
-    list_of_number[index],list_of_number[min-index]=list_of_number[index]
-print(list_of_number)
+list_of_number=[52,6562,5455,555,2202,52]
+print(bubble_sort(list_of_number))
