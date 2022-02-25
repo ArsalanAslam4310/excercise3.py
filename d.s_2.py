60c27599811fd5d3979271fcb93224a85fbcc4b9
ArsalanAslam4310 <97877778+ArsalanAslam4310@users.noreply.github.com>
2022-02-25 11:17:21 +0500

    Update d.s_2.py

--- d.s_2.py
+++ d.s_2.py
@@ -1,5 +1,8 @@
-lis = [11, 45, 8, 23, 14, 12, 78, 45, 89]
+lis = [11, 45, 8, 23, 14, 12, 78, 45, 89,8]
+lis1 = lis[0:len(lis)//3]
+lis2 = lis[len(lis)//3:(len(lis)//3)*2]
+lis3 = lis[(len(lis)//3)*2:len(lis)]
 
-if lis==lis[0:3]:
-    lis=lis[::-1]
-print(lis)
+print(lis1)
+print(lis2)
+print(lis3)
