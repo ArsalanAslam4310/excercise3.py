fc1ddceecae3aea33480801e942a79dd0151d731
ArsalanAslam4310 <97877778+ArsalanAslam4310@users.noreply.github.com>
2022-02-28 23:31:06 +0500

    Add files via upload

---
+++ chp_3_ex_3.py
@@ -0,0 +1,13 @@
+for i in range(3):
+    print("+",end=" - - - - ")
+    print("+",end=" - - - - +")
+     
+    for j in range(4):
+        print("         ","!","       ","!")
+        print("!")
+print("+ - - - - + - - - - +")
+
+        
+        
+
+
