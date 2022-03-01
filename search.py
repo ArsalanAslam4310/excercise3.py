d48645a7ed5ce3764e31cb5235c121ad617e607f
ArsalanAslam4310 <97877778+ArsalanAslam4310@users.noreply.github.com>
2022-03-01 23:01:06 +0500

    Update search.py

--- search.py
+++ search.py
@@ -1,12 +1,15 @@
 def search_from_list(lis,targets):
+    
     for i in range(len(lis)):
         if lis[i]==targets:
             return i
-        
-    
-    
+        for j in range(1,len(targets)-1):
+            if targets[j]==lis[i]:
+                print(targets)
+                return j
+      
 
-list_of_numbers=[5,4,7,8,1,2,10,55,44]
-target=[4,7,8]
-search=search_from_list(list_of_numbers,target)
+lis=[5,4,7,8,1,2,10,55,44]
+target=[7,8,1]
+search=search_from_list(lis,target)
 print(search)
