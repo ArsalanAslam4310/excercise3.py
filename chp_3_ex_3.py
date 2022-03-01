644c96842eb5812c609ae4e2c08305894d0fe86e
ArsalanAslam4310 <97877778+ArsalanAslam4310@users.noreply.github.com>
2022-03-01 22:56:58 +0500

    Update chp_3_ex_3.py

--- chp_3_ex_3.py
+++ chp_3_ex_3.py
@@ -1,11 +1,14 @@
-for i in range(3):
-    print("+",end=" - - - - ")
-    print("+",end=" - - - - +")
+def draw():
+    for i in range(3):
+        print("+",end=" - - - - ")
+        print("+",end=" - - - - +")
      
-    for j in range(4):
-        print("         ","!","       ","!")
-        print("!")
-print("+ - - - - + - - - - +")
+        for j in range(4):
+            print("         ","!","       ","!")
+            print("!")
+    print("+ - - - - + - - - - +")
+    
+draw()
 
         
         
