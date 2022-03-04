45c447d4bcfd50320c6fe66bd581f45e9344a320
ArsalanAslam4310 <97877778+ArsalanAslam4310@users.noreply.github.com>
2022-03-04 07:04:02 +0500

    Add files via upload

--- search.py
+++ search.py
@@ -1,15 +1,16 @@
 def search_from_list(lis,targets):
     
+
     for i in range(len(lis)):
         if lis[i]==targets:
             return i
-        for j in range(1,len(targets)-1):
+        for j in range(0,len(targets)-1):
             if targets[j]==lis[i]:
-                print(targets)
-                return j
+                return i
+                
       
 
-lis=[5,4,7,8,1,2,10,55,44]
-target=[7,8,1]
+lis=[5,6,7,88,1,2,10,55,44]
+target=[1,2,10]
 search=search_from_list(lis,target)
 print(search)
