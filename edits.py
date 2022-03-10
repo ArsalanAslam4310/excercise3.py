72b854739f2e72e256e1716f80b9066545adc992
ArsalanAslam4310 <97877778+ArsalanAslam4310@users.noreply.github.com>
2022-03-10 07:01:06 +0500

    Update edits.py

--- edits.py
+++ edits.py
@@ -1,11 +1,10 @@
 
+
 def edit(lis,exist,new_occurence):
     for i in range(len(lis)):
         if lis[i]==exist:
-            new_occurence=exist
+            lis[i]=new_occurence
             return lis
 
-            
-
 lis=[6,5,4,9,3,8,9]
-print(edit(lis,3,90))
+print(edit(lis,8,56))
